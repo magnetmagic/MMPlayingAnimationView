ff6912bf50aeaa2603ea212a965c63a41afe4f1f
your name <your@email.com>
2016-11-11 14:34:24 +0900

    Initial Commit

---
+++ PlayingAnimationView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PlayingAnimationView
+//
+//  Created by matsubaratomoki on 2016/11/11.
+//  Copyright © 2016年 magnet-magic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
